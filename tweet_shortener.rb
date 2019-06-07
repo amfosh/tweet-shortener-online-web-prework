f4d76d7ec0a96e7536451b3f016970e77bd413cf
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-07 20:37:19 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def dictionary(item)
+  dictionary = {"hello" => 'hi',
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"}
+  
+  tweets.each do |x,y|
+    if (x==item)
+      return y
+    end  
+ end
+ item
+end 
+
+def word_substituter(string)
+  string.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+   end.join(" ")
+end 
+
